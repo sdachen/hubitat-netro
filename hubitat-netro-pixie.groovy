d4cfeae6c1bc41655ddcf9963a066a2e60e251e7
Scott Deeann Chen <scott.deeann.chen@gmail.com>
2023-01-07 15:10:32 -0800

    Add Netro Pixie

--- hubitat-netro-pixie.groovy
+++ hubitat-netro-pixie.groovy
@@ -17,9 +17,9 @@ metadata {
     definition (name: "Netro Pixie Smart Hose Faucet Timer", namespace: "sdachen.netro", author: "Scott Deeann Chen") {
 
         command "refresh"
-        command "water"
-        command "cancelAll"
-        command "stopWatering"
+        command "waterMinutes", ["number"]
+        command "pauseWateringDays", ["number"]
+        command "cancelManualSchedules"
 
         attribute "batteryLevel", "double"
         attribute "isActiveToday", "boolean"
@@ -28,6 +28,7 @@ metadata {
         attribute "nextEndTimeUtc", "string"
         attribute "nextStartTimeLocal", "string"
         attribute "nextEndTimeLocal", "string"
+        attribute "nextDateLocal", "string"
 
     }
 
@@ -51,25 +52,28 @@ void updated() {
 
 void refresh() {
     if (enableLogging) log.debug "refresh()"
-    asyncGetSchedulesData()
+    asyncGetSchedules()
 }
 
-void water() {
+void waterMinutes(BigDecimal duration) {
     if (enableLogging) log.debug "water()"
+    asyncPostWater(duration)
 }
 
-void cancelAll() {
-    if (enableLogging) log.debug "cancelAll()"
+void pauseWateringDays(BigDecimal days) {
+    if (enableLogging) log.debug "pauseWateringDays()"
+    asyncPostNoWater(days)
 }
 
-void stopWatering() {
-    if (enableLogging) log.debug "stopWatering()"
+void cancelManualSchedules() {
+    if (enableLogging) log.debug "cancelManualSchedules()"
+    asyncPostStopWater()
 }
 
 @Field static final String netroURI = "https://api.netrohome.com"
 
-void asyncGetSchedulesData() {
-    if (enableLogging) log.debug "asyncGetSchedulesData()"
+void asyncGetSchedules() {
+    if (enableLogging) log.debug "asyncGetSchedules()"
 
     def today = new Date(now())
     def todayString = today.format("yyyy-MM-dd")
@@ -83,39 +87,99 @@ void asyncGetSchedulesData() {
         path: "/npa/v1/schedules.json",
         query: query]
 
-    asynchttpGet("getScheduleCallback", params)
-    runIn((int)pollingInterval * 60, asyncGetSensorData)
+    asynchttpGet("parseResponseSchedule", params)
+    runIn((int)pollingInterval * 60, asyncGetSchedules)
 }
 
-void getScheduleCallback(response, responseData) {
+void parseResponseSchedule(response, responseData) {
     if (enableLogging) log.debug "getScheduleCallback()"
+    if (enableLogging) log.debug response.getStatus()
     if (response.getStatus() == 200) {
-        def json = response.getJson()
-        def data = json.data.schedules
+        def currentTime = new Date(now())
 
-        if (data.size > 0) {
-            def lastData = data[0]
-            def lastDateTime = toDateTime(lastData.start_time + "Z")
+        def json = response.getJson()
+        def schedules = json.data.schedules.findAll { toDateTime(it.start_time + "Z").after(currentTime) }
+        
+        if (schedules.size > 0) {
             
-            for (int i = 0; i < data.size; ++i) {
-                def dateTime = toDateTime(data[i].start_time + "Z")
-                if (lastDateTime.after(dateTime)) {
-                    lastDateTime = dateTime
-                    lastData = data[i]
+            def firstSchedule = schedules[0]
+            def firstDateTime = toDateTime(firstSchedule.start_time + "Z")
+            
+            for (int i = 0; i < schedules.size; ++i) {
+                def dateTime = toDateTime(schedules[i].start_time + "Z")
+                if (firstDateTime.after(dateTime)) {
+                    firstDateTime = dateTime
+                    firstSchedule = schedules[i]
                 }
             }
 
-            sendEvent(name: "nextStartTimeUtc", value: lastData.start_time, descriptionText: "Next Start Time UTC", isStateChange: false)
-            sendEvent(name: "nextEndTimeUtc", value: lastData.end_time, descriptionText: "Next End Time UTC", isStateChange: false)
-            sendEvent(name: "nextStartTimeLocal", value: lastData.local_start_time, descriptionText: "Next Start Time Local", isStateChange: false)
-            sendEvent(name: "nextEndTimeLocal", value: lastData.local_end_time, descriptionText: "Next End Time Local", isStateChange: false)
-            sendEvent(name: "scheduleSource", value: lastData.source, descriptionText: "Schedule source", isStateChange: false)
+            sendEvent(name: "nextStartTimeUtc", value: firstSchedule.start_time, descriptionText: "Next Start Time UTC", isStateChange: false)
+            sendEvent(name: "nextEndTimeUtc", value: firstSchedule.end_time, descriptionText: "Next End Time UTC", isStateChange: false)
+            sendEvent(name: "nextStartTimeLocal", value: firstSchedule.local_start_time, descriptionText: "Next Start Time Local", isStateChange: false)
+            sendEvent(name: "nextEndTimeLocal", value: firstSchedule.local_end_time, descriptionText: "Next End Time Local", isStateChange: false)
+            sendEvent(name: "scheduleSource", value: firstSchedule.source, descriptionText: "Schedule source", isStateChange: false)
+            sendEvent(name: "nextDateLocal", value: firstSchedule.local_date, descriptionText: "Next local date.", isStateChange: false)
+            sendEvent(name: "isActiveToday", value: true, descriptionText: "Data available.", isStateChange: false)
         } else {
-            sendEvent(name: "isActiveToday", value: false, descriptionText: "No data last 24 hours.", isStateChange: false)
+            sendEvent(name: "isActiveToday", value: false, descriptionText: "No data.", isStateChange: false)
         }
-
     } else {
         if (enableLogging) log.debug "Request Error!"
         sendEvent(name: "isActiveToday", value: false, descriptionText: "Request Error.", isStateChange: false)
     }
+}
+
+void asyncPostWater(BigDecimal duration) {
+    if (enableLogging) log.debug "asyncPostWater()"
+    
+    Map data = [
+        key: serialNumber, 
+        duration: duration]
+
+    Map params = [
+        uri: netroURI,
+        path: "/npa/v1/water.json",
+        requestContentType: 'application/json',
+		contentType: 'application/json',
+        body: data]
+    
+    asynchttpPost("parseResponseSchedule", params)
+}
+
+void asyncPostStopWater() {
+    if (enableLogging) log.debug "asyncPostStopWater()"
+    
+    Map data = [
+        key: serialNumber]
+
+    Map params = [
+        uri: netroURI,
+        path: "/npa/v1/stop_water.json",
+        requestContentType: 'application/json',
+		contentType: 'application/json',
+        body: data]
+    
+    asynchttpPost("callbackGetSchedules", params)
+}
+
+void asyncPostNoWater(BigDecimal days) {
+    if (enableLogging) log.debug "asyncPostStopWater()"
+    
+    Map data = [
+        key: serialNumber,
+        days: days]
+
+    Map params = [
+        uri: netroURI,
+        path: "/npa/v1/no_water.json",
+        requestContentType: 'application/json',
+		contentType: 'application/json',
+        body: data]
+    
+    asynchttpPost("callbackGetSchedules", params)
+}
+
+void callbackGetSchedules(response, data) {
+    asyncGetSchedules()
+
 }
