0c0d750d61743f5466e0e36ee753f960e4b6be71
Scott Deeann Chen <aweather@gmail.com>
2023-05-15 22:12:21 -0700

    Add last water time. Add dummy capability.

--- hubitat-netro-whisperer.groovy
+++ hubitat-netro-whisperer.groovy
@@ -14,6 +14,7 @@ import groovyx.net.http.ContentType
 
 metadata {
     definition (name: "Netro Whisperer Smart Plant Sensor", namespace: "sdachen.netrowhisperer", author: "Scott Deeann Chen") {
+        
         capability "TemperatureMeasurement"
         capability "IlluminanceMeasurement"
 
